df543fe8aa2e6ecddb7d5110127f9c37eaf58986
Alex Stelea <alex.stelea@gmail.com>
2013-10-17 18:02:30 -0400

    Added models

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,10 +5,23 @@ import play.api.mvc._
 
 object Application extends Controller {
 
-  def index = Action {
-    Redirect(routes.Application.signIn)
-  }
+	val loginForm = Form(
+		tuple(
+			"email"  -> text,
+			"password" -> text
+			) verifying ("Invalid email or password", result => result match {
+	  			case (email, password) => User.authenticate(email, password).isDefined
+			})
+		)
 
-  def signIn = TODO
+	def login = Action { implicit request => 
+		Ok(html.login(loginForm))
+	}
+
+	def index = Action {
+		Redirect(routes.Application.signIn)
+	}
+
+	def signIn = TODO
 
 }
