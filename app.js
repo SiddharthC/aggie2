54a4b224ab3ef6f89697aaa3608809bb10ad47d6
Natraj Kaushik <natraj.iitb@gmail.com>
2013-11-12 14:54:35 -0500

    Cleaned up app.js. Moved all the REST API call handlers and HTML page serving callbacks to controllers/api.js.
    Signed-off-by: Natraj Kaushik <natraj.iitb@gmail.com>

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var config = require("./config.js");
 
 var User = require('./models/user.js');
 var Controller = require('./controllers/api.js');
-
-var nodemailer = require('nodemailer');
 var _ = require("underscore");
 
 var app = express();
@@ -14,29 +12,7 @@ var app = express();
 /* connect to Mongo when the app initializes */
 mongoose.connect(config.DATABASE_URL);
 
-app.configure(function() { //Admin account init check.
-	User.findOne({
-		username: "admin"
-	}, function(err, result) {
-		if (err) {
-			throw err;
-		}
-
-		if (!result) {
-			new User({
-				username: "admin",
-				password: "adminadmin",
-				email: "admin@domain.com",
-				isAdminFlag: "true"
-			}).save(function(err, result) {
-				if (err) {
-					throw err;
-				}
-			});
-		}
-	});
-
-});
+app.configure(Controller.configure);
 
 app.use(express.json());
 app.use(express.urlencoded());
@@ -69,153 +45,19 @@ var controller = {
 	}
 };
 
-/* Global function for session authentication */
-var authenticate = function(req, res, next) {
-	var isAuthenticated = false;
-	if (req.session.username) {
-		isAuthenticated = true;
-	}
-	if (isAuthenticated) {
-		next();
-	} else {
-		console.log("Authentication error");
-		res.redirect('/login');
-	}
-};
-
-var authenticateAdmin = function(req, res, next) {
-	var isAuthenticated = false;
-	if (req.session.username && req.session.isAdminFlag === true)
-		isAuthenticated = true;
-	if (isAuthenticated)
-		next();
-	else {
-		console.log("Admin verification failed");
-		res.redirect('/login');
-	}
-}
-
 /* Redirect to login page */
 app.get("/", function(req, res) {
-	res.redirect('/login');
+	res.redirect("/login");
 });
 
 
 /* serve the registration page */
-app.get("/register", authenticateAdmin, function(req, res) {
-	res.sendfile(__dirname + '/views/register.html');
-	req.session.lastPage = '/login';
-});
-
-app.get("/login", function(req, res) {
-	res.sendfile(__dirname + '/views/login.html');
-	req.session.lastPage = '/login';
-});
-
-app.get("/home", authenticate, function(req, res, next) {
-	res.sendfile(__dirname + '/views/home.html');
-});
-
-app.get("/start-crawler", authenticate, function(req, res, next) {
-	res.sendfile(__dirname + '/views/start-crawler.html');
-	//res.send("Test");
-});
-
-app.post("/register", authenticateAdmin, function(req, res, next) {
-	var username = req.param("username", "");
-	var password = req.param("password", "");
-	var email = req.param("email", "");
-	var isAdminFlag = req.param("isAdminFlag", "");
-
-	if (username == 'Username') {
-		res.send("Please input a vaild username");
-		return;
-	}
-
-	//TODO make a regex check for valid email
-	if (username == 'mail@address.com') {
-		res.send("Please input a vaild email address");
-		return;
-	}
-
-	if (isAdminFlag === "") {
-		isAdminFlag = false;
-	}
-
-	User.findOne({
-		email: email
-	}, function(err, result) {
-		if (err) {
-			throw err;
-		}
-
-		if (result) {
-			res.send("This email id is already registered...\nPlease use Reset Form for password recovery.");
-		} else {
-			new User({
-				username: username,
-				password: password,
-				email: email,
-				isAdminFlag: isAdminFlag
-			}).save(function(err, result) {
-				if (err) {
-					throw err;
-				}
-
-				// setup e-mail data with unicode symbols
-				var mailOptions = {
-					from: "Aggie 2.0 Dev Team <aggie.node@gmail.com>", // sender address
-					to: email, // list of receivers
-					subject: "Hello World", // Subject line
-					text: "Hello world", // plaintext body
-					html: "<b>Hello world.</b>" // html body
-				}
-
-				// send mail with defined transport object
-				smtpTransport.sendMail(mailOptions, function(error, response) {
-					if (error) {
-						console.log("Invalid Email: " + error);
-						res.redirect('/register');
-					}
-					//smtpTransport.close(); // shut down the connection pool, no more messages
-				});
-
-				res.send("Please verify through the email received.");
-			});
-		}
-	});
-});
-
-
-app.post("/login", function(req, res) {
-	var username = req.param("username", "");
-	var password = req.param("password", "");
-	User.findOne({
-		username: username
-	}, function(err, user) {
-		if (err) {
-			throw err;
-		}
-		if (user) {
-
-			user.comparePassword(password, function(err, isMatch) {
-				if (err) {
-					throw err;
-				}
-
-				if (isMatch) { //check if password match
-					req.session.username = username; //set session username for the session. Used for authentication.
-					req.session.isAdminFlag = user.isAdminFlag;
-					res.redirect('/home');
-				} else {
-					res.redirect('/login');
-				}
-			});
-		} else {
-			res.redirect('/login');
-		}
-	});
-});
+app.get("/register", Controller.helper.authenticateAdmin, Controller.registerPage);
+app.get("/login", Controller.loginPage);
+app.get("/home", Controller.helper.authenticate, Controller.home);
+app.get("/start-crawler", Controller.helper.authenticate, Controller.startBot);
+app.post("/register", Controller.helper.authenticateAdmin, Controller.register);
+app.post("/login", Controller.login);
 
 
 /* Handle request to start a crawler */
@@ -224,14 +66,5 @@ app.post("/stop-twitter-bot", _.bind(Controller.stopTwitterBot, {controller: con
 
 /* Handle GET request for feed */
 app.get("/feed", Controller.feed);
-
-
-var smtpTransport = nodemailer.createTransport("SMTP", {
-	service: "Gmail",
-	auth: {
-		user: "aggie.node@gmail.com",
-		pass: "freeNfair"
-	}
-});
 
 app.listen(config.SERVER_PORT);
--- controllers/api.js
+++ controllers/api.js
@@ -1,6 +1,7 @@
 var User = require('../models/user.js');
 var Data = require("../models/data.js");
 var TwitterBotController = require("./bots/twitterbot/twitter-bot-controller.js");
+var nodemailer = require('nodemailer');
 
 exports.createUser = function(req, res) {
 	var email = req.param("email", "");
@@ -27,7 +28,178 @@ exports.authenticate_user = function(req, res){
 	});
 };
 
+var smtpTransport = nodemailer.createTransport("SMTP", {
+	service: "Gmail",
+	auth: {
+		user: "aggie.node@gmail.com",
+		pass: "freeNfair"
+	}
+});
+
 var Controller = {
+
+	helper : {
+		authenticate : function(req, res, next) {
+			var isAuthenticated = false;
+			if (req.session.username) {
+				isAuthenticated = true;
+			}
+			if (isAuthenticated) {
+				next();
+			} else {
+				console.log("Authentication error");
+				res.redirect('/login');
+			}
+		},
+
+		authenticateAdmin : function(req, res, next) {
+			var isAuthenticated = false;
+			if (req.session.username && req.session.isAdminFlag === true)
+				isAuthenticated = true;
+			if (isAuthenticated)
+				next();
+			else {
+				console.log("Admin verification failed");
+				res.redirect('/login');
+			}
+		}
+	},
+
+	configure : function() { //Admin account init check.
+		User.findOne({
+			username: "admin"
+		}, function(err, result) {
+			if (err) {
+				throw err;
+			}
+
+			if (!result) {
+				new User({
+					username: "admin",
+					password: "adminadmin",
+					email: "admin@domain.com",
+					isAdminFlag: "true"
+				}).save(function(err, result) {
+					if (err) {
+						throw err;
+					}
+				});
+			}
+		});
+	},
+	
+	loginPage : function(req, res) {
+		res.sendfile("login.html", {root: "./views/"});
+		req.session.lastPage = '/login';
+	},
+
+	home : function(req, res, next) {
+		res.sendfile("home.html", {root: "./views/"});
+	},
+
+	startBot : function(req, res, next) {
+		res.sendfile("start-crawler.html", {root: "./views/"});
+	},
+
+	registerPage : function(req, res) {
+		res.sendfile("register.html", {root: "./views/"});
+		req.session.lastPage = '/login';
+	},
+
+	register : function(req, res, next) {
+		var username = req.param("username", "");
+		var password = req.param("password", "");
+		var email = req.param("email", "");
+		var isAdminFlag = req.param("isAdminFlag", "");
+
+		if (username == 'Username') {
+			res.send("Please input a vaild username");
+			return;
+		}
+
+		//TODO make a regex check for valid email
+		if (username == 'mail@address.com') {
+			res.send("Please input a vaild email address");
+			return;
+		}
+
+		if (isAdminFlag === "") {
+			isAdminFlag = false;
+		}
+
+		User.findOne({
+			email: email
+		}, function(err, result) {
+			if (err) {
+				throw err;
+			}
+
+			if (result) {
+				res.send("This email id is already registered...\nPlease use Reset Form for password recovery.");
+			} else {
+				new User({
+					username: username,
+					password: password,
+					email: email,
+					isAdminFlag: isAdminFlag
+				}).save(function(err, result) {
+					if (err) {
+						throw err;
+					}
+
+					// setup e-mail data with unicode symbols
+					var mailOptions = {
+						from: "Aggie 2.0 Dev Team <aggie.node@gmail.com>", // sender address
+						to: email, // list of receivers
+						subject: "Hello World", // Subject line
+						text: "Hello world", // plaintext body
+						html: "<b>Hello world.</b>" // html body
+					};
+
+					// send mail with defined transport object
+					smtpTransport.sendMail(mailOptions, function(error, response) {
+						if (error) {
+							console.log("Invalid Email: " + error);
+							res.redirect('/register');
+						}
+						//smtpTransport.close(); // shut down the connection pool, no more messages
+					});
+
+					res.send("Please verify through the email received.");
+				});
+			}
+		});
+	},
+
+	login : function(req, res) {
+		var username = req.param("username", "");
+		var password = req.param("password", "");
+		User.findOne({
+			username: username
+		}, function(err, user) {
+			if (err) {
+				throw err;
+			}
+			if (user) {
+
+				user.comparePassword(password, function(err, isMatch) {
+					if (err) {
+						throw err;
+					}
+
+					if (isMatch) { //check if password match
+						req.session.username = username; //set session username for the session. Used for authentication.
+						req.session.isAdminFlag = user.isAdminFlag;
+						res.redirect('/home');
+					} else {
+						res.redirect('/login');
+					}
+				});
+			} else {
+				res.redirect('/login');
+			}
+		});
+	},
 
 	/* Current implementation is to simply return top 10 most recent results from the Data collection */
 	feed : function(req, res) {
