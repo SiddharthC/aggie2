3539616d187533048b9f11caefef85d14ce00b59
sesgoe <jsesgoe@gmail.com>
2013-11-21 14:47:48 -0500

    Added dynamic message prepending to messages page
    Added dynamic message prepending to messages page
    
    Need to add: Pagination, tweet username, tweet handler, and image if
    possible

---
+++ views/scripts/messages.js
@@ -0,0 +1,39 @@
+console.log("here");
+
+$(document).ready(function() {
+	var SERVER_URL = "/feed";
+	var REFRESH_PERIOD = 30000;
+
+	var getFeedData = function(){
+		$.ajax({
+			url: SERVER_URL,
+			type: "GET"
+		}).done(function(data) {
+			console.log(data);
+			for(var i = 0; i < data.length; i++){
+				generateHTML(data[i].message);
+			}
+		});
+	};
+	
+	var generateHTML = function(message) {
+		var rowHTML = 
+		"<div class=\"message\">"
+					+ "<div class=\"messageleft\">"
+						+ "<input type=\"checkbox\" name=\"checkID\" value=\"checkID\" style=\"margin: 20px 0 0 24px; padding:0;\">"
+						+ "<p>5 min</p>"
+					+"</div>"
+					+"<div class=\"messageright\">"
+						+"<img src=\"images/twitpic.png\" height=\"60\" width=\"60\" title=\"twitpic\" alt=\"twitpic\" />"
+						+"<div class=\"tweet\">"
+							+"<h1>ImATwitterBot <i>@TwitterBot</i></h1>"
+							+"<h2>" + message + "</h2>"
+						+"</div>"
+					+"</div>"
+               +"</div>";
+			   
+		$("#messagecontainer").prepend(rowHTML);
+	};
+	getFeedData();
+	setInterval(getFeedData, REFRESH_PERIOD);
+});
