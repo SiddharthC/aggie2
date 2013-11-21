10af5d6e451d2362c290a60e82b580d5f70001f5
sesgoe <jsesgoe@gmail.com>
2013-11-21 15:52:30 -0500

    Working on adding fade in
    working on adding fade-in,

--- views/scripts/messages.js
+++ views/scripts/messages.js
@@ -3,6 +3,7 @@ console.log("here");
 $(document).ready(function() {
 	var SERVER_URL = "/feed";
 	var REFRESH_PERIOD = 30000;
+	var ID = 0;
 
 	var getFeedData = function(){
 		$.ajax({
@@ -17,8 +18,9 @@ $(document).ready(function() {
 	};
 	
 	var generateHTML = function(message) {
+		var currentID = ID;
 		var rowHTML = 
-		"<div class=\"message\">"
+		"<div class=\"message\" id=\"message_\"" + currentID + ">"
 					+ "<div class=\"messageleft\">"
 						+ "<input type=\"checkbox\" name=\"checkID\" value=\"checkID\" style=\"margin: 20px 0 0 24px; padding:0;\">"
 						+ "<p>5 min</p>"
@@ -33,6 +35,8 @@ $(document).ready(function() {
                +"</div>";
 			   
 		$("#messagecontainer").prepend(rowHTML);
+		$("#message_" + currentID).fadeIn();
+		ID++;
 	};
 	getFeedData();
 	setInterval(getFeedData, REFRESH_PERIOD);
